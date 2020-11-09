7b993471a8e52dfa2df556a23869143737cbe067
Yen-Tse Liao <s88037zz@gmail.com>
2020-11-09 09:03:03 +0800

    add the simple http request

--- just_buy/src/App.js
+++ just_buy/src/App.js
@@ -10,17 +10,7 @@ import Search from './components/Search/search'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/"> Home </Link></li>
-            <li><Link to="/"> About </Link></li>
-            <li><Link to="/"> Users </Link></li>
-          </ul>
-        </nav>
-      </div>
-      
+    <Router>      
       <Switch>
         <Route path="/" component={Search} />
         <Route path='/search' component={Search}> </Route>
--- just_buy/src/components/Search/search.js
+++ just_buy/src/components/Search/search.js
@@ -3,21 +3,71 @@ import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
+
+const serverConfig = {
+    url: "http://127.0.0.1:5000/search",
+    data: "?key=1&item=3"
+}
+
+const center = {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+}
+const nav_bar_style ={
+    background: "#A9A9A9"
+}
 class Search extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            count: 0
+        }
+
+    }
+    addCount = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+        console.log(this.state.count)
+    }
+    saerch(){
+        fetch(serverConfig.url+serverConfig.data, {
+            method: "GET",
+        }).then(res=>{
+            return res.json()
+        }).then(data=>{
+            console.log(data)
+        })  
+    }
+
     render(){
         return(
-            <div className={{root: {flexGrow: 1}}}> 
-                <h1>This is Search</h1>
-                <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                        <form>
-                            <TextField id="serach-keyword" label="Keyword" />
-                        </form>
+            <div  className={{root: {flexGrow: 1}}} style={nav_bar_style}> 
+                <Grid container justify="center" spacing={2} align='center'>
+                     <Grid item xs={1}>
+
                     </Grid>
-                    <Grid item xs={6}>
-                        <IconButton variant="contained" color="primary" >
-                            <SearchIcon />
-                        </IconButton>
+                    <Grid item xs={3}>
+                        <div>
+                            <h1>Just Buy</h1>
+                        </div>
+                    </Grid>
+                    <Grid item xs={4} >
+                        <div style={{marginTop: 10}}>
+                            <form>
+                                <TextField id="serach-keyword" label="Keyword" fullWidth/>
+                            </form>
+                        </div>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <div style={{marginTop:20}}>
+                            <IconButton variant="contained" color="primary" onClick={this.saerch}>
+                                <SearchIcon />
+                            </IconButton>
+                        </div>
+                    </Grid>
+                    <Grid item xs={2}>
                     </Grid>
                 </Grid>
             </div>
